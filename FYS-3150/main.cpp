f1c765ff964c581331fc566ab7bfb292e923eac0
Thomas Sjåstad <thomassjastad@1x-193-157-253-125.uio.no>
2017-08-30 11:40:15 +0200

    Initial Commit

---
+++ FYS-3150/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FYS-3150
+//
+//  Created by Thomas Sjåstad on 30.08.2017.
+//  Copyright © 2017 Thomas Sjåstad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

e829b00068581b282d0ec359190f0337797981c1
Thomassjaastad <Thomassjaastad@users.noreply.github.com>
2017-09-07 14:42:14 +0200

    Add files via upload

--- Project1.py
+++ Project1.py
@@ -1,15 +1,13 @@
 from __future__ import division
 from matplotlib.pyplot import *
 from numpy import *
+from numpy.linalg import solve
 
-N = 4
-
-h = 1/(N+1)
-a = -1
+N = 1000        #Gridpoints
+h = 1/(N+1)    #Step length
+a = -1         #Values for Matrix elements
 b = 2
 c = -1
-
-#x = [i*h for i in range(N)]
 
 #Analytic solution
 def U(x):
@@ -17,17 +15,18 @@ def U(x):
 
 #Numerical solution
 def f(x):
-	return 100*exp(-10*x)
+	f = 100*exp(-10*x) 
+	return f 
 
 #Defining all arrays needed for storing values 
 #Vektor v for Av = B
 #Vektor B og A for Av = B
-d_twidd = zeros(N)
-f_twidd = zeros(N)
+d_twidd = zeros(N+2)
+f_twidd = zeros(N+2)
 
 x = zeros(N+2)
-B = zeros(N)
-A = zeros((N,N))
+B = zeros(N+2)
+A = zeros((N+2,N+2))
 
 #Conditions for x-array and numerical solution Av = B 
 
@@ -40,37 +39,48 @@ v[N+1] = 0
 
 #Producing x-array
 
-for j in range(1, N):
+for j in range(N+1):
 	x[j] = j*h 
 
 #Filling A with 2's on diag and -1 on diags below and above diag
 #Setting values for B
-for i in range(N):
+
+f_twidd[0] = h**2*f(x[0])  #Init value for row reduction
+d_twidd[0] = 2        #Init value for row reduction
+d_twidd[N+1] = 2
+
+for i in range(N+2):
+
 	A[i][i] = b
-	A[i][i-1] = a
-	A[i-1][i] = c
-	B[i] = h**2*f(x[i])
-A[0][i] = 0
-A[i][0] = A[0][i]
+	if i!=0 :
+		A[i][i-1] = a
+	if i!=(N+2):
+		A[i-1][i] = c
+	A[N+1][0] = 0
+	f_twidd[i] = h**2*f(x[i])
 
-f_twidd[0] = f(x[0]) #Init value for row reduction
-d_twidd[0] = 2 #Init value for row reduction
+#Diag elements
+for k in range(1, N+1):
+	d_twidd[k] = (k+1.)/k
 
-#A[0][N-1] = 0
-#A[N-1][0] = 0
+#Forward sub
+for k in range(2, N+1):
+	f_twidd[k] = h**2*f(x[k]) + f_twidd[k-1]/d_twidd[k-1]
 
-for k in range(1,N-1):
-	d_twidd[k] = (k+1)/k
-	f_twidd[k] = f(x[k]) + ((k-1)*f_twidd[k-1])/k
-	v[k-1] = ((k-1)/k)*(f_twidd[k-1] + v[k])
 
-v[k] = f_twidd[k]/d_twidd[k]
-print v
+v[N] = f_twidd[N]/d_twidd[N]
 
-#plot(x, f(x), label = 'Analytic sol')
-#xlabel('$x$', fontsize = 16)
-#ylabel('y', fontsize = 16)
+#Backward sub
+for k in reversed(range(2, N+1)):
+	v[k-1] = (f_twidd[k-1] + v[k])/d_twidd[k-1]
 
-#hold('on')
-#plot(x, v, label = 'Num sol')
-#show()
+t = linspace(0,1,100)
+
+plot(t, U(t), label = 'Analytic sol U(x)')
+hold('on')
+plot(x, v, label = 'Num sol (v(x))')
+xlabel('$x$', fontsize = 18)
+ylabel('$y$', fontsize = 18)
+title('$Possion$ $equation,$ $analytic$ $and$ $numerical$', fontsize = 20)
+legend()
+show()
